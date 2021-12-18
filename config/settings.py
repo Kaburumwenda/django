91e7748e007a6e82770425f430c42d3dce7af220
Kaburumwenda <yuskaburu@gmail.com>
2021-12-18 05:45:40 -0500

    aws build spec added

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'apps.api_services',
+    'products',
 ]
 
 MIDDLEWARE = [
